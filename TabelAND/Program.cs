81bd237b482b169f5c4e0fcdf56e65bfe8eb02e6
Pohuș Tabita <emanuela.pohus@emanuel.ro>
2024-01-20 17:16:30 -1200

    Tabel adevăr

---
+++ TabelAND/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using System.Runtime.Intrinsics;
+
+namespace TabelAND
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabel de adevar pentru functia SI (AND):");
+            Console.WriteLine("|  A  |  B  |  A AND B  |");
+            Console.WriteLine("|-----|-----|-----------|");
+
+            for (int a = 0; a <= 1; a++)
+            {
+                for (int b = 0; b <= 1; b++)
+                {
+                    bool result = (a == 1) && (b == 1);
+                    Console.WriteLine($"|  {a}  |  {b}  |     {result}     |");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
